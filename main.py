a19c5e01bc13d18afa093159055df9a424806e10
Tenuka <tenukaperera1025@gmail.com>
2026-02-09 22:07:58 +0530

    initial webcam imports and initialization

--- main.py
+++ main.py
@@ -1,16 +1,13 @@
 import cv2
+import mediapipe as mp
+from mediapipe.tasks import python
+from mediapipe.tasks.python import visio
 
 cap = cv2.VideoCapture(0)
 
-while True:
-    success, frame = cap.read()
-    if not success:
-        break
+while(0):
+    frame = cap.read()
 
-    cv2.imshow("Camera Test", frame)
+    HSV = cv2.cvtColor(frame, cv2.COLOR_BAYER_BG2BGR)
 
-    if cv2.waitKey(1) & 0xFF == ord('q'):
-        break
-
-cap.release()
-cv2.destroyAllWindows()
+    
