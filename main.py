49d64b177078c92f50d9727b696d75e5c88dae5c
Tenuka <tenukaperera1025@gmail.com>
2026-02-14 21:36:11 +0530

    new function and function call added

--- main.py
+++ main.py
@@ -86,9 +86,14 @@ def print_result(result: HandLandmarkerResult, output_image: mp.Image, timestamp
        movement_fingers(CMC=IndexFinger_DIP, MCP=IndexFinger_MCP,IP=IndexFinger_PIP,TIP=IndexFinger_Tip)
        movement_fingers(CMC=MiddleFinger_DIP,MCP=MiddleFinger_MCP,IP=MiddleFinger_PIP,TIP=MiddleFinger_Tip)
        movement_fingers(CMC=RingFinger_DIP,MCP=RingFinger_MCP,IP=MiddleFinger_PIP,TIP=RingFinger_Tip)
-       movement_fingers(CMC=PinkyFinger_DIP,MCP=PinkyFinger_MCP,IP=MiddleFinger_PIP,TIP=PinkyFinger_Tip)
-
-
+       movement_fingers(CMC=PinkyFinger_DIP,MCP=PinkyFinger_MCP,IP=PinkyFinger_PIP,TIP=PinkyFinger_Tip)
+       OutputLandmarks(thumb_tip,frames=output_image.numpy_view())
+  
+def OutputLandmarks(Tip,frames):
+    height,width,channels = frames.shape
+    x_pixel  = int(Tip.x * width)
+    y_pixel = int(Tip.y* height)
+    cv2.circle(frame, (x_pixel, y_pixel), 8, (0, 0, 255), -1)
 
 
 options = HandLandmarkerOptions( 
